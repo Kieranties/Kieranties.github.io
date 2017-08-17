39e6156bd43bcd33b1fea73f5f70850a1a23884f
Kieranties <Kieran@Kieranties.com>
2017-08-17 21:03:00 +0100

    Use vscode tasks because we can

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-add-repository ppa:brightbox/ruby-ng
-sudo apt-get update
-sudo apt-get install ruby2.3 ruby2.3-dev build-essential
-sudo gem update –system
-sudo gem install jekyll bundler
