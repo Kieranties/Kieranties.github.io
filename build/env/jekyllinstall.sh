83d3d01108abb918ad7994f196495247838117d3
Kieran Marron <Kieran.marron@sitecore.com>
2018-03-14 20:31:21 +0000

    Working with remote theme

--- build/env/jekyllinstall.sh
+++ build/env/jekyllinstall.sh
@@ -10,6 +10,12 @@ sudo apt-add-repository ppa:brightbox/ruby-ng
 sudo apt-get update
 sudo apt-get install ruby2.3 ruby2.3-dev build-essential dh-autoreconf
 
+# Additonal requirements for nokogiri
+sudo apt-get install zlib1g-dev
+
+# Additional requirements for github-pages
+sudo apt-get install libcurl4-openssl-dev
+
 # Update gems
 sudo gem update
 
